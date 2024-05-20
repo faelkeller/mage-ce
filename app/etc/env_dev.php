1d88a37a09d3e9b8d6c664f1bf209eb16b9e35de
Rafael Keller <rafael.keller@infobase.com.br>
2024-05-20 19:02:51 -0300

    First commit magento 2.4.7 ce

---
+++ app/etc/env_dev.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'MmmeCDx4dMcLcnHX84DFjMPLvPyzJP0k'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f53_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f53_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base648/F2EB3/1rky4a77ys4u9rp4sWv1UpdP4q3uPmu0M3g='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento2',
+                'username' => 'magento2',
+                'password' => 'magento2',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'magento2.docker'
+    ],
+    'install' => [
+        'date' => 'Thu, 25 Apr 2024 13:51:19 +0000'
+    ]
+];
